fdffae86aa2d43dd0e183616bfacb2e00762cfec
Sergiogd112 <sergio.gomezdamas@gmail.com>
2020-12-01 14:44:19 +0100

    update

--- fib.cpp
+++ fib.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main()
 {
-    cout << "test" << endl;
+    int n = 40;
+    long long int fib[n];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i < n; i++)
+    {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    cout << "Fibonachi of " << n ;
+    cout << " with c++ is " << fib[n-1] << endl;
 }
--- fib.py
+++ fib.py
@@ -1,4 +1,4 @@
-n = 100
+n = 40
 print("Fibonachi of", n," with python is ",end='')
 fib = [0, 1]
 [fib.append(fib[x - 1] + fib[x - 2]) for x in range(2, n)]
